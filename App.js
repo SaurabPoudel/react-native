e8014eb05f40e3919859d5b7176fb5a97b30b206
SaurabPoudel <poudelsaurab20@gmail.com>
2023-11-03 20:07:17 +0545

    My first react native

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ScrollView,Button, Pressable } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "black", padding: 60 }}>
+      <ScrollView>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Text style={{ color: "white" }}>
+        k chha ipsum dolor sit amet consectetur adipisicing elit. Porro
+        aspernatur ex quam iste harum hic tempore? Neque accusantium in deleniti
+        ullam quasi, dolores tenetur officia autem explicabo, numquam enim.
+        Necessitatibus et veniam, cupiditate, impedit, quo quibusdam veritatis
+        expedita id eaque mollitia accusamus provident itaque. Id quaerat omnis
+        nobis maiores? Nesciunt error tempore optio distinctio enim, natus quam,
+        culpa veritatis quisquam tenetur quas quibusdam illo in dolorem odit,
+        blanditiis debitis voluptas libero recusandae. Consequatur nam cumque
+        ipsum culpa, quis dicta deleniti nesciunt quidem omnis mollitia
+        voluptatem aperiam quaerat placeat alias odio vel voluptates? Sequi
+        consequuntur praesentium earum recusandae et in numquam deserunt, nam
+        provident a mollitia tempora similique blanditiis nostrum quis quibusdam
+        porro distinctio! Impedit eaque maxime illo, magnam saepe ipsam, animi
+        ex rem, iste sunt dolor deserunt modi veniam numquam tempora architecto
+        aspernatur vitae officia error veritatis autem minus praesentium sint
+        incidunt. Recusandae inventore impedit velit officiis ut ab harum atque
+        enim assumenda? Itaque molestias beatae quibusdam, alias ut architecto
+        nulla adipisci illo ipsam, dolorem maxime, exercitationem ullam soluta
+        excepturi illum dolores cupiditate officia ab eligendi commodi! Modi,
+        dolores quia optio voluptas aliquid consequuntur, explicabo rerum velit
+        reprehenderit perferendis eveniet repudiandae voluptatibus fugit
+        consequatur assumenda quas praesentium dolorem reiciendis ipsum.
+      </Text>
+      {/* <ImageBackground source={logoImg} style={{flex: 1}}>
+       <Text style={{color: "white"}}> Hello World</Text>
+    </ImageBackground> */}
+    <Pressable onPress={()=> console.log("Image pressed")} onLongPress={()=> console.log("Long pressed")} onPressOut={()=> console.log("Long pressed sucessfully")}>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+    </Pressable>
+      <Button title="Press" onPress={()=> console.log("Button Pressed")} color="midnightblue"/>
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
